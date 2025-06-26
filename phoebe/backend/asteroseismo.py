Adding tolerances to account for poles in xi_theta and xi_phi (and the related derivatives of Y wrt theta), the mathematical poles theta=0 and theta=pi were not handled correctly; fixed.

phoebe/backend/asteroseismo.py
@@ -12,6 +12,7 @@ try:
 except:
     from scipy.special import factorial
 
+
 def as_legendre(l, m, x):
     """
     @l: non-radial degree (the number of longitudinal and latitudinal node lines)
@@ -29,14 +30,15 @@ def as_legendre(l, m, x):
     m_ = abs(m)
     legendre_poly = legendre(l)
     deriv_legpoly_ = legendre_poly.deriv(m=m_)
-    deriv_legpoly = np.polyval(deriv_legpoly_,x)
+    deriv_legpoly = np.polyval(deriv_legpoly_, x)
     P_l_m = (-1)**m_ * (1-x**2)**(m_/2.) * deriv_legpoly
     if m < 0:
         P_l_m = (-1)**m_ * factorial(l-m_)/factorial(l+m_) * P_l_m
     return P_l_m
 
+
 def as_Y(l, m, theta, phi):
-    """
+    r"""
     @l: non-radial degree (the number of longitudinal and latitudinal node lines)
     @m: longitudinal order (the number of longitudinal node lines)
     @theta: colatitude, 0 at the +z pole, pi at the -z pole
@@ -51,6 +53,7 @@ def as_Y(l, m, theta, phi):
     Plm = as_legendre(l, m, np.cos(theta))
     return factor*Plm*np.exp(1j*m*phi)
 
+
 def as_norm_J(l, m):
     """
     @l: non-radial degree (the number of longitudinal and latitudinal node lines)
@@ -61,12 +64,13 @@ def as_norm_J(l, m):
 
     return np.sqrt(float((l**2-m**2))/(4*l**2-1)) if abs(m) < l else 0
 
+
 def as_norm_atlp1(l, m, Omega, k):
     """
     @l: non-radial degree (the number of longitudinal and latitudinal node lines)
     @m: longitudinal order (the number of longitudinal node lines)
     @Omega: spin parameter (Omega_rot/omega_freq)
-    @k:
+    @k: ratio of the horizontal to radial displacement
 
     Normalization factor for the l+1 term.
     """
@@ -75,12 +79,13 @@ def as_norm_atlp1(l, m, Omega, k):
         return 0.0
     return Omega * (l-abs(m)+1)/(l+1) * 2./(2*l+1) * (1-l*k)
 
+
 def as_norm_atlm1(l, m, Omega, k):
     """
     @l: non-radial degree (the number of longitudinal and latitudinal node lines)
     @m: longitudinal order (the number of longitudinal node lines)
     @Omega: spin parameter (Omega_rot/omega_freq)
-    @k:
+    @k: ratio of the horizontal to radial displacement
 
     Normalization factor for the l-1 term.
     """
@@ -88,6 +93,7 @@ def as_norm_atlm1(l, m, Omega, k):
     if Omega < 1e-6:
         return 0.0
     return Omega * (l+abs(m))/l * 2./(2*l+1) * (1 + (l+1)*k)
+
 
 def as_dYdtheta(l, m, theta, phi):
     """
@@ -102,13 +108,13 @@ def as_dYdtheta(l, m, theta, phi):
     """
 
     if abs(m) >= l:
-        Y = 0.
-    else:
-        factor = 1./np.sin(theta)
-        term1 = l     * as_norm_J(l+1, m) * as_Y(l+1, m, theta, phi)
-        term2 = (l+1) * as_norm_J(l,   m) * as_Y(l-1, m, theta, phi)
-        Y = factor * (term1 - term2)
-    return Y
+        return 0.
+
+    factor = 1./np.sin(theta)
+    term1 = l * as_norm_J(l+1, m) * as_Y(l+1, m, theta, phi)
+    term2 = (l+1) * as_norm_J(l,   m) * as_Y(l-1, m, theta, phi)
+    return factor * (term1 - term2)
+
 
 def as_dYdphi(l, m, theta, phi):
     """
@@ -124,8 +130,9 @@ def as_dYdphi(l, m, theta, phi):
 
     return 1j*m*as_Y(l, m, theta, phi)
 
+
 def as_xi_r(l, m, theta, phi, wt):
-    """
+    r"""
     @l: non-radial degree (the number of longitudinal and latitudinal node lines)
     @m: longitudinal order (the number of longitudinal node lines)
     @theta: colatitude, 0 at the +z pole, pi at the -z pole
@@ -139,48 +146,63 @@ def as_xi_r(l, m, theta, phi, wt):
 
     return as_Y(l, m, theta, phi) * np.exp(1j*wt)
 
-def as_xi_theta(l, m, theta, phi, wt, Omega=0, k=0):
-    """
+
+def as_xi_theta(l, m, theta, phi, wt, Omega=0, k=0, pole_tolerance=1e-6):
+    r"""
     @l: non-radial degree (the number of longitudinal and latitudinal node lines)
     @m: longitudinal order (the number of longitudinal node lines)
     @theta: colatitude, 0 at the +z pole, pi at the -z pole
     @phi: azimuth, 0 in the x direction, goes from 0 to 2pi
     @wt: omega*t, where omega=2 \pi \nu is the angular frequency, and t is time
     @Omega: spin parameter
-    @k:
+    @k: ratio of the horizontal to radial displacement
+    @pole_tolerance: tolerance for the mathematical pole at theta=0
+        or pi, to avoid division by zero
 
     Unscaled co-latitudinal displacement, see
         Aerts et al. (2010), the Asteroseismology book and
         Zima W, A&A 455, 227–234 (2006), Appendix A
     """
 
+    # Avoid division by zero
+    theta[theta < pole_tolerance] = pole_tolerance
+    theta[theta > np.pi-pole_tolerance] = np.pi-pole_tolerance
+
     term1 = k * as_dYdtheta(l, m, theta, phi) * np.exp(1j*wt)
     term2 = as_norm_atlp1(l, m, Omega, k) / np.sin(theta) * as_dYdphi(l+1, m, theta, phi) * np.exp(1j*wt + np.pi/2)
     term3 = as_norm_atlm1(l, m, Omega, k) / np.sin(theta) * as_dYdphi(l-1, m, theta, phi) * np.exp(1j*wt - np.pi/2)
     return term1 + term2 + term3
 
-def as_xi_phi(l, m, theta, phi, wt, Omega=0, k=0):
-    """
+
+def as_xi_phi(l, m, theta, phi, wt, Omega=0, k=0, pole_tolerance=1e-6):
+    r"""
     @l: non-radial degree (the number of longitudinal and latitudinal node lines)
     @m: longitudinal order (the number of longitudinal node lines)
     @theta: colatitude, 0 at the +z pole, pi at the -z pole
     @phi: azimuth, 0 in the x direction, goes from 0 to 2pi
     @wt: omega*t, where omega=2 \pi \nu is the angular frequency, and t is time
     @Omega: spin parameter
-    @k:
+    @k: ratio of the horizontal to radial displacement
+    @pole_tolerance: tolerance for the mathematical pole at theta=0
+        or pi, to avoid division by zero
 
     Unscaled longitudinal displacement, see
         Aerts et al. (2010), the Asteroseismology book and
         Zima W, A&A 455, 227–234 (2006), Appendix A
     """
 
+    # Avoid division by zero
+    theta[theta < pole_tolerance] = pole_tolerance
+    theta[theta > np.pi-pole_tolerance] = np.pi-pole_tolerance
+
     term1 = k/np.sin(theta) * as_dYdphi(l, m, theta, phi) * np.exp(1j*wt)
     term2 = -as_norm_atlp1(l, m, Omega, k) * as_dYdtheta(l+1, m, theta, phi)*np.exp(1j*wt+np.pi/2)
     term3 = -as_norm_atlm1(l, m, Omega, k) * as_dYdtheta(l-1, m, theta, phi)*np.exp(1j*wt-np.pi/2)
     return term1 + term2 + term3
 
-if __name__=='__main__':
-    print('Checking whether negative m values are computed correctly:'),
+
+if __name__ == '__main__':
+    print('Checking whether negative m values are computed correctly:')
     ls, x = [0, 1, 2, 3, 4, 5], np.cos(np.linspace(0, np.pi, 100))
     check = 0
     for l in ls:
@@ -188,7 +210,7 @@ if __name__=='__main__':
             Ppos = as_legendre(l,  m, x)
             Pneg = as_legendre(l, -m, x)
             mycheck = Pneg, (-1)**m * factorial(l-m)/factorial(l+m) * Ppos
-            check += sum(abs(mycheck[0]-mycheck[1])>1e-10)
+            check += sum(abs(mycheck[0]-mycheck[1]) > 1e-10)
     if check == 0:
         print('yes')
     else: